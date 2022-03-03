2f45226ecba851efdfd584f33053110d96a229e6
Alexander Garro Diaz <agarrodiaz@hotmail.com>
2022-03-02 23:23:43 -0600

    Web

--- WebApp/App_Start/ContainerExtensions.cs
+++ WebApp/App_Start/ContainerExtensions.cs
@@ -1,17 +1,19 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using BD;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WBL;
 
 namespace WebApp
 {
     public static class ContainerExtensions
     {
-
         public static IServiceCollection AddDIContainer(this IServiceCollection services)
         {
-
+            services.AddSingleton<IDataAccess, DataAccess>();
+            services.AddTransient<IParametroService, ParametroService>();
 
             return services;
         }
