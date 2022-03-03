18cc135c2b1885787d7196c2389f7a2c38772590
Alexander Garro Diaz <agarrodiaz@hotmail.com>
2022-03-02 20:51:14 -0600

    Service

---
+++ BaseDeDatos/dbo/StoreProcedures/ParametroObtener.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE [dbo].[ParametroObtener]
+	@Id_Parametro INT = NULL
+
+AS 
+BEGIN 
+  SET NOCOUNT ON
+  
+  SELECT 
+		Id_Parametro
+		,Codigo
+		,Descripcion
+		,Valor
+		,Estado
+  FROM dbo.Parametro
+  WHERE
+	   (@Id_Parametro IS NULL OR Id_Parametro=@Id_Parametro)
+END
---
+++ Entity/dbo/ParametroEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ParametroEntity: DBEntity
+    {
+        public int? Id_Parametro { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string Valor { get; set; }
+        public int? Estado { get; set; }
+    }
+}
